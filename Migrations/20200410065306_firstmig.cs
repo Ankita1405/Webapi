fd45c73a53b90fccfa278bc55e637fe0d965ded3
Ankita1405 <ankusharrma007.as@gmail.com>
2020-04-10 14:35:18 +0530

    initialcommit

---
+++ Migrations/20200410065306_firstmig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CustomerApi.Migrations
+{
+    public partial class firstmig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    CId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CName = table.Column<string>(nullable: true),
+                    CDob = table.Column<DateTime>(nullable: false),
+                    CPIN = table.Column<int>(maxLength: 4, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.CId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customer",
+                columns: new[] { "CId", "CDob", "CName", "CPIN" },
+                values: new object[] { 1, new DateTime(2019, 12, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "sam", 1234 });
+
+            migrationBuilder.InsertData(
+                table: "Customer",
+                columns: new[] { "CId", "CDob", "CName", "CPIN" },
+                values: new object[] { 2, new DateTime(2019, 12, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "sami", 4562 });
+
+            migrationBuilder.InsertData(
+                table: "Customer",
+                columns: new[] { "CId", "CDob", "CName", "CPIN" },
+                values: new object[] { 3, new DateTime(2019, 12, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), "sameer", 7894 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customer");
+        }
+    }
+}
---
+++ Models/CustomerContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerApi.Models
+{
+    public class CustomerContext:DbContext
+    {
+        public CustomerContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Customer>Customers { get; set; }
+        protected override void OnModelCreating
+    (ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasData(
+                new Customer { CId = 1,CName = "sam", CDob = new DateTime(day: 02, month: 12, year: 2019),CPIN =1234 },
+                  new Customer { CId = 2, CName = "sami", CDob = new DateTime(day: 03, month: 12, year: 2019), CPIN =4562},
+                    new Customer { CId = 3, CName = "sameer", CDob= new DateTime(day: 04, month: 12, year: 2019), CPIN =7894}
+                );
+        }
+
+
+    }
+}
